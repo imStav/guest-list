c13d3d5a300e22f1979333f681d949a144b5d818
imStav <saprocontact@gmail.com>
2023-01-24 15:48:58 +0100

    Added a `button` to add a guest to the list
    Implemented a guest description that contains the infos, and dynamic classes that are applied depending on the gender of the guest.

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ const name = ref("");
 const gender = ref(GENDER.MALE);
 
 interface Guest {
-  // id: number,
+  id: number;
   name: string;
   gender: GENDER;
 }
@@ -20,6 +20,7 @@ const guestList = ref<Guest[]>([]);
 const addGuest = (): void => {
   if (name.value) {
     guestList.value.push({
+      id: Math.floor(Math.random() * 1000),
       name: name.value,
       gender: gender.value,
     });
@@ -36,12 +37,29 @@ const addGuest = (): void => {
       <h1>Guests</h1>
     </div>
 
-    <input type="text" placeholder="Name" @keypress.enter="addGuest" />
-    <div>{{ guestList }}</div>
-    <select v-model="gender" @keypress.enter="addGuest">
+    <input type="text" placeholder="Name" v-model.trim="name" />
+
+    <select v-model="gender">
       <option :value="GENDER.MALE">{{ GENDER.MALE }}</option>
       <option :value="GENDER.FEMALE">{{ GENDER.FEMALE }}</option>
     </select>
+
+    <button @click="addGuest">Add guest</button>
+
+    <div v-for="guest in guestList" :key="guest.id">
+      <div class="guest_description">
+        <p class="guest_name">{{ guest.name }}</p>
+        <p
+          :class="
+            guest.gender === GENDER.MALE
+              ? 'guest_gender_male'
+              : 'guest_gender_female'
+          "
+        >
+          {{ guest.gender }}
+        </p>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -59,15 +77,38 @@ select {
   color: currentColor;
   border: 1px solid currentColor;
   padding: 0.5rem;
+  width: 50%;
 }
 
 input {
-  width: 50%;
   margin-block: 0.5rem;
 }
 
 select {
-  width: 50%;
   margin-bottom: 2rem;
+}
+
+button {
+  width: 50%;
+  border: none;
+  padding-block: 0.5rem;
+  background-color: #833ded;
+  color: whitesmoke;
+}
+
+.guest_description {
+  display: flex;
+}
+
+.guest_description p:nth-child(2) {
+  margin-left: 1rem;
+}
+
+.guest_gender_male {
+  color: deepskyblue;
+}
+
+.guest_gender_female {
+  color: violet;
 }
 </style>
